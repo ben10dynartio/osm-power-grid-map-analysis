957f4a1ec62d297b4aa7513ae174f7b8a9bd47a7
B.Ribon <b.ribon@dynartio.com>
2025-12-09 11:20:22 +0100

    Experiment standalone automation

---
+++ qgis/standalone-automation.py
@@ -0,0 +1,34 @@
+import sys
+
+print(sys.path)
+
+from qgis.core import QgsApplication
+
+
+# Supply path to qgis install location
+
+QgsApplication.setPrefixPath("/path/to/qgis/installation", True)
+
+
+# Create a reference to the QgsApplication.  Setting the
+
+# second argument to False disables the GUI.
+
+qgs = QgsApplication([], False)
+
+
+# Load providers
+
+qgs.initQgis()
+
+
+# Write your code here to load some layers, use processing
+
+# algorithms, etc.
+
+
+# Finally, exitQgis() is called to remove the
+
+# provider and layer registries from memory
+
+qgs.exitQgis()
